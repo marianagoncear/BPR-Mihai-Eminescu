de6590579eaf8198a5d112efd4510be9618c6a35
marianagoncear <144808572+marianagoncear@users.noreply.github.com>
2024-04-23 18:39:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+new SimpleLightbox({elements: ".galerie-container a"});
+
+new SimpleLightbox({elements: ".orar-imagini a"});
+
+AOS.init({ once: true });
+
+emailjs.init("y08DWiUO4pGyzv6RG");
+
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+
+    if (x.className === "") {
+        x.className = "mobile";
+    } else {
+        x.className = "";
+    }
+}
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_tavzzgi", "template_td6wjcn", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+
+}
